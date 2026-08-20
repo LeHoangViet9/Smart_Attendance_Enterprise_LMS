616ddf2e4746cf5a01f800c31ac2977100c21472
LeHoangViet9 <leviet20051509@gmail.com>
2026-08-20 15:25:33 +0700

    first commit

---
+++ src/main/java/edufit_com_lms/SmartAttendanceEnterpriseLmsApplication.java
@@ -0,0 +1,13 @@
+package edufit_com_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceEnterpriseLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAttendanceEnterpriseLmsApplication.class, args);
+    }
+
+}
